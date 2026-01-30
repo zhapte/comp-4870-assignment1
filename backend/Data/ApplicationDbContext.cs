34cd1f704a83f79e4e72e896d079cc6b0a33ae7c
Lucas Liu <lucasqhliu@gmail.com>
2026-01-30 14:41:20 -0800

    initial commit

---
+++ backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
