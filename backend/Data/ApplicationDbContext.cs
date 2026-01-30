29649ac0f2ea65ec99497b2b3fbe430948440a7e
Lucas Liu <lucasqhliu@gmail.com>
2026-01-30 15:02:05 -0800

    fixed the controller

--- backend/Data/ApplicationDbContext.cs
+++ backend/Data/ApplicationDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using backend.Models;
 
 namespace backend.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
 {
+    public DbSet<Article> Article { get; set; }
 }
